c7161e0e4bf7e9700862c980f1f340de6aad0a00
Ayush Pratap Singh <ayushpratap344@gmail.com>
2022-12-08 14:46:51 +0530

    Additional Files Added

---
+++ index.js
@@ -0,0 +1,57 @@
+// Requiring module
+const dotenv = require('dotenv')
+dotenv.config()
+const discord = require('discord.js')
+const request = require('request')
+const fetch = require('node-fetch')
+//https://codeforces.com/api/contest.list?gym=true
+
+// Creates a discord client
+const client = new discord.Client()
+const url = "https://codeforces.com/api/contest.list"
+
+let options = { json: true }
+
+
+// Runs whenever a message is sent
+client.on('message', (message) => {
+    // Checks if the message says "hello"
+    if (message.content === 'contest') {
+
+        request(url, options, (error, res, body) => {
+            if (error) {
+                return console.log(error)
+            }
+
+            if (!error && res.statusCode == 200) {
+
+                let response = ` `;
+                const arr1 = res.body.result
+                let i = 0;
+                while (true) {
+                    if (arr1[i].phase === "BEFORE") {
+                        let mydate = new Date(arr1[i].startTimeSeconds * 1000)
+                        mydate = mydate.toLocaleString('hi-IN', { timeZone: 'Asia/Kolkata' })
+                        response += "Contest Name - " + arr1[i].name + '\n'
+                        response += 'Timing- ' + mydate + '\n'
+                        response += "Duration - " + (arr1[i].durationSeconds / 3600) + " hours \n"
+                        response += "Type - " + (arr1[i].type) + '\n\n'
+
+                    }
+                    else {
+                        break;
+                    }
+                    i = i + 1;
+                }
+
+                message.channel.send(response)
+
+            }
+        })
+
+    }
+})
+process.env.API_KEY = 'MTA1MDMxMjMwMzI3NjQ1ODAwNA.Gmnufe.s7Ir3G519WHtZRswjxqhwHckUeAdHLck4wSQyY';
+client.login(process.env.API_KEY)
+
+
